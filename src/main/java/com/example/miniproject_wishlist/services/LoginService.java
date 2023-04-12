01409b7599074e02a9892842ce85b6024842f726
PrinceH00 <hamz0230@stud.kea.dk>
2023-04-12 10:48:33 +0200

    changed to userRepo in loginService

--- src/main/java/com/example/miniproject_wishlist/services/LoginService.java
+++ src/main/java/com/example/miniproject_wishlist/services/LoginService.java
@@ -1,21 +1,20 @@
 package com.example.miniproject_wishlist.services;
 
-import com.example.miniproject_wishlist.dto.LoginDTO;
 import com.example.miniproject_wishlist.models.User;
-import com.example.miniproject_wishlist.repositories.IWishlistRepository;
+import com.example.miniproject_wishlist.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    private final IWishlistRepository repository;
+    private final IUserRepository userRepository;
 
     public LoginService(ApplicationContext context, @Value("${wishlist.repository.impl}") String impl) {
-        repository = (IWishlistRepository) context.getBean(impl);
+        userRepository = (IUserRepository) context.getBean(impl);
     }
 
     public User getUser(String email, String password){
-        return repository.getUser(email, password);
+        return userRepository.getUser(email, password);
     }
 }
